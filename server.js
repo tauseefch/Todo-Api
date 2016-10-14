0ad4085b66422a98168931c7ce1e4d5114af7183
Tauseef Chaudhary <m.tauseef23@yahoo.com>
2016-10-14 23:53:40 +0500

    Refactor Api with _

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -18,12 +20,7 @@ app.get('/todos', function(req, res ) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
-	todos.forEach(function (todo) {
-		if(todoID === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoID});
 	if(matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -33,10 +30,12 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
-	//add id filed
+	var body = _.pick(req.body, 'description', 'completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
-	//push body into array
 	todos.push(body);
 	res.json(body);
 });
