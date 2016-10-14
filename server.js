352e46904c9fb2081a2eeb7f7d8ed5c7d1369b00
Tauseef Chaudhary <m.tauseef23@yahoo.com>
2016-10-14 22:43:42 +0500

    Add post for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet Mom fo lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed the cat',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo Api Root');
@@ -39,6 +31,15 @@ app.get('/todos/:id', function(req, res) {
 	}
 });
 
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	//add id filed
+	body.id = todoNextId++;
+	//push body into array
+	todos.push(body);
+	res.json(body);
+});
 
 app.listen(PORT, function() {
 	console.log('Express listing on port ' + PORT + '!');
