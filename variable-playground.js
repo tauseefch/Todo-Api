f9bb034b486fd641c7ac5c0ccf7ed4c894eb23b1
Tauseef Chaudhary <m.tauseef23@yahoo.com>
2016-10-15 08:47:10 +0500

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// }
+
+// function updatePerson (obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+
+var grades = [15, 28];
+
+function addGrades (grades) {
+	grades.push(55);
+}
+
+addGrades(grades);
+console.log(grades);
