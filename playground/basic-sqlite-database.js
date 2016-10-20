21a20ec2af7da17f6a1c794c6a2ecd6e3ac48352
Tauseef <Muneeb.me@gmail.com>
2016-10-20 12:57:14 +0500

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -17,41 +17,36 @@ var Todo = sequelize.define('todo', {
 		allowNull: false,
 		defaultValue: false
 	}
-})
+});
+
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
 sequelize.sync().then(function() {
 	console.log('Everything is synced');
-	Todo.findById(3).then(function (todo) {
-		if(todo){
-			console.log(todo.toJSON());
-		} else {
-			console.log('todo not found');
-		}
-	}); 
-	// Todo.create({
-	// 	description: 'Take out the trash'
-	// }).then(function(todo) {
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function (todos){
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
+	});
+	// User.create({
+	// 	email: 'tauseef@example.com'
+	// }).then(function () {
 	// 	return Todo.create({
-	// 		description: 'Clean Office'
+	// 	 description: 'Clean yard'
 	// 	});
-	// }).then(function(){
-	// 	// return Todo.findById(1)
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%trash%'
-	// 			}
-	// 		}
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
 	// 	});
-	// }).then(function(todos) {
-	// 	if(todos){
-	// 		todos.forEach(function(todo) {
-	// 		console.log(todo.toJSON());	
-	// 		})
-			
-	// 	} else{
-	// 		console.log('No to do found');
-	// 	}
-	// }).catch(function (e) {
-	// 	console.log(e);
 	// });
 });
--- server.js
+++ server.js
@@ -61,7 +61,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	//call create on db.todo
 	db.todo.create(body).then(function(todo) {
-		res.json(todo.toJSON());
+		req.user.addTodo(todo).then(function () {
+			return todo.reload();
+		}).then(function (todo) {
+			res.json(todo.toJSON());
+		});
 	}, function(e) {
 		res.status(400).json(e);
 	});
